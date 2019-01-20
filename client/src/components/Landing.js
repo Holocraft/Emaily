fc502cd3441662ab5ef1ebe867c1a1abf280e73d
Holocraft <perrin1057@gmail.com>
2019-01-20 12:22:37 -0800

    added link to landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
     <div style={{ textAlign: 'center' }}>
       <h1>Emaily</h1>
-      Collect feedback from your users
+      <p>Collect feedback from your users</p>
+      <Link to="/surveys" className="waves-effect waves-light btn-large">
+        Get Started
+      </Link>
     </div>
   );
 };
